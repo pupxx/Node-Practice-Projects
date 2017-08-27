c8876ddadb8b2faf5812ae1f8d287b15870090f3
Cheryln <cherylnbarber@gmail.com>
2017-08-26 17:26:45 -0700

    Initial commit

---
+++ notes-app/app.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const _ = require('lodash');
+
+const notes = require('./notes.js')
+
+// fs.appendFile('hello.txt', 'Good Evening Cheryln', (err)=>{
+//   if(err) throw err;
+//   console.log('Data has been appended');
+// })
+
+console.log('goodbye');
+
