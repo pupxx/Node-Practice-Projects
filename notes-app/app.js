a569707070da97e2ebe547401ad84f968c7d558f
Cheryln <cherylnbarber@gmail.com>
2017-08-31 10:18:33 -0700

    Added yargs. Added functions for add, list, read, and remove that simply log to the console for now.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,12 +1,23 @@
 const fs = require('fs')
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js')
 
-// fs.appendFile('hello.txt', 'Good Evening Cheryln', (err)=>{
-//   if(err) throw err;
-//   console.log('Data has been appended');
-// })
+const argv = yargs.argv;
+var command = argv._[0]
 
-console.log('goodbye');
+console.log('command ', command);
+console.log('yargs ', argv);
 
+if (command === 'add') {
+    notes.addNote(argv.title, argv.body)
+} else if(command === 'list') {
+    notes.getAll();
+}else if(command === 'read'){
+    notes.read(argv.title);    
+}else if(command === 'remove'){
+    notes.remove(argv.title)    
+}else{
+    console.log('Command not recognized');
+}
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,17 @@
+function addNote (title, body){
+    console.log('Adding note ', title, body);
+}
 
+function getAll() {
+    console.log('Getting All files');
+}
 
+function read(title) {
+    console.log('Reading the file', title);
+}
 
+function remove(title) {
+    console.log('Removing the file with title: ', title);
+}
 
-module.exports = {}
+module.exports = {addNote, getAll, read, remove}
