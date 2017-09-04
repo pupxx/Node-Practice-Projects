bdad76e91343a60ce1fb85c945d5dd48d8d87780
Cheryln <cherylnbarber@gmail.com>
2017-09-03 21:05:06 -0700

    Refactored

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -2,67 +2,64 @@
 const fs = require('fs')
 
 function listNotes(){
-    console.log('Listing All Notes');
-    var noteString = fs.readFileSync('data.json')
-    var notes = JSON.parse(noteString)
+    var notes = fetchNotes();
     notes.forEach((el)=>{
         console.log(el.title + '\n' + el.body + '\n\n');
     })
 }
 
 function addNote(title, body){
-    var notes = [];
+    var notes = fetchNotes();
     var note = {
         title,
         body
-    }
-    
-    try {
-        var noteString = fs.readFileSync('data.json')
-        notes = JSON.parse(noteString)
-    } catch (error) {
-        console.log(error);
-    }
-    
-    if(checkForDuplicates(notes, title).length === 0){
-        console.log(`Adding note with title: ${title}, and body of: ${body} `);
+    }    
+    if(checkForTitle(notes, title).length === 0){
         notes.push(note);
-        fs.writeFileSync('data.json', JSON.stringify(notes))
+        sendNote(notes)
     }else{
         console.log('That title already exists.  Please try another one');
     }
 }
 
 function removeNote(title){
-    console.log(`removing note with title: ${title}`);
-    var notesString = fs.readFileSync('data.json');
-    var notes = JSON.parse(notesString) 
+    var notes = fetchNotes()
     var index;
     notes.forEach(function(el, i) {
       if(el.title === title){
+          console.log(i);
           index = i
       }  
     });
-    console.log(index);
     notes.splice(index, 1)
-    console.log(notes);
-    fs.writeFileSync('data.json', JSON.stringify(notes))
+    sendNote(notes)
 }
 
 function getNote(title){
-    console.log(`Reading note with title: ${title}`);
-    var notesString = fs.readFileSync('data.json');
-    var notes = JSON.parse(notesString)   
-    var note = notes.filter((el)=>{
-        return el.title === title;
-    })
+    var notes = fetchNotes();
+    var note = checkForTitle(notes, title);
     console.log(note[0].body);
 }
 
-function checkForDuplicates(array, title){
+// ****************  HELPER FUNCTIONS *********************
+
+function fetchNotes(){
+    try {
+        var noteString = fs.readFileSync('data.json')
+        return JSON.parse(noteString)
+    } catch (error) {
+        return []
+    }
+}
+
+function checkForTitle(array, title){
   return array.filter((el)=>{
     return el.title === title
   })
+}
+
+function sendNote(array){
+    fs.writeFileSync('data.json', JSON.stringify(array))
 }
 
 module.exports = {addNote, listNotes, removeNote, getNote}
