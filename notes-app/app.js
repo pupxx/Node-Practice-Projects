785f7233d1d799aebb883a7738aee3c5584277bc
Cheryln <cherylnbarber@gmail.com>
2017-09-03 17:45:40 -0700

    Added Yargs Added fs Added function to add a note to the data.json file only if no duplicates.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const _ = require('lodash');
+const yargs = require('yargs').argv
 
 const notes = require('./notes.js')
 
@@ -8,5 +9,16 @@ const notes = require('./notes.js')
 //   console.log('Data has been appended');
 // })
 
-console.log('goodbye');
+var command = yargs._[0]
 
+if(command === 'list'){
+    notes.listNotes()
+}else if(command === 'add'){
+    notes.addNote(yargs.title, yargs.body)  
+}else if(command === 'read'){
+    notes.getNote(yargs.title)
+}else if(command === 'remove'){
+    notes.removeNote(yargs.title)
+}else {
+    console.log('Please choose to either List, Add, or Remove a note');    
+}
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,45 @@
 
+const fs = require('fs')
 
+function listNotes(){
+    console.log('Listing All Notes');
+}
 
+function addNote(title, body){
+    var notes = [];
+    var note = {
+        title,
+        body
+    }
+    
+    try {
+        var noteString = fs.readFileSync('data.json')
+        notes = JSON.parse(noteString)
+    } catch (error) {
+        console.log(error);
+    }
+    
+    if(checkForDuplicates(notes, title).length === 0){
+        console.log(`Adding note with title: ${title}, and body of: ${body} `);
+        notes.push(note);
+        fs.writeFileSync('data.json', JSON.stringify(notes))
+    }else{
+        console.log('That title already exists.  Please try another one');
+    }
+}
 
-module.exports = {}
+function removeNote(title){
+    console.log(`removing note with title: ${title}`);
+}
+
+function getNote(title){
+    console.log(`Reading note with title: ${title}`);
+}
+
+function checkForDuplicates(array, title){
+  return array.filter((el)=>{
+        return el.title === title
+  })
+}
+
+module.exports = {addNote, listNotes, removeNote, getNote}
