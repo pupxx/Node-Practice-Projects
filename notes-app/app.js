f4ddb4c065dca832dfc513f7323916de5c0279cd
Cheryln <cherylnbarber@gmail.com>
2017-09-06 09:35:57 -0700

    Continued refactoring

--- notes-app/app.js
+++ notes-app/app.js
@@ -4,37 +4,34 @@ const yargs = require('yargs').argv
 
 const notes = require('./notes.js')
 
-// fs.appendFile('hello.txt', 'Good Evening Cheryln', (err)=>{
-//   if(err) throw err;
-//   console.log('Data has been appended');
-// })
+var command = yargs._[0];
 
-var command = yargs._[0]
-
-if(command === 'list'){
-   var noteList = notes.listNotes() 
-   if(noteList.length > 0){
-       noteList.forEach((el) => {
-           console.log(el.title + '\n' + el.body + '\n\n');
-       })
-   }  
-   console.log('There are no notes to list'); 
-}else if(command === 'add'){
+if (command === 'list') {
+    var noteList = notes.listNotes()
+    console.log(`Printing ${noteList.length} notes. \n`);
+    if (noteList.length > 0) {
+        noteList.forEach((el) => {
+            console.log(el.title + '\n' + el.body + '\n\n');
+        })
+    } else {
+        console.log('There are no notes to list');
+    };
+} else if (command === 'add') {
     var note = notes.addNote(yargs.title, yargs.body);
     if (note) {
         console.log('Note was created Successfully');
         console.log(`Title: ${note.title}`);
-    }else {
+    } else {
         console.log('This title is already taken');
     }
-}else if(command === 'read'){
+} else if (command === 'read') {
     var note = notes.getNote(yargs.title)
     var message = note ? `${note[0].title}\n\n${note[0].body}` : 'Note note found'
     console.log(message);
-}else if(command === 'remove'){
+} else if (command === 'remove') {
     var item = notes.removeNote(yargs.title)
     var message = item ? "Item was successuflly removed" : "Title does not exist"
     console.log(message);
-}else {
-    console.log('Please choose to either List, Add, or Remove a note');    
+} else {
+    console.log('Please choose to either List, Add, or Remove a note');
 }
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,37 +1,37 @@
 
 const fs = require('fs')
 
-function listNotes(){
+function listNotes() {
     var notes = fetchNotes();
     return notes
 }
 
-function addNote(title, body){
+function addNote(title, body) {
     var notes = fetchNotes();
     var note = {
         title,
         body
-    }    
-    if(checkForTitle(notes, title).length === 0){
+    }
+    if (checkForTitle(notes, title).length === 0) {
         notes.push(note);
         sendNote(notes);
         return note
     }
 }
 
-function removeNote(title){
+function removeNote(title) {
     var notes = fetchNotes()
     var index = [];
 
-    var newArray = notes.filter((el)=> el.title !== title)
+    var newArray = notes.filter((el) => el.title !== title)
     sendNote(newArray)
-    if(newArray.length === notes.length){
+    if (newArray.length === notes.length) {
         return false
     }
     return true
 }
 
-function getNote(title){
+function getNote(title) {
     var notes = fetchNotes();
     var note = checkForTitle(notes, title);
     return note
@@ -39,7 +39,7 @@ function getNote(title){
 
 // ****************  HELPER FUNCTIONS *********************
 
-function fetchNotes(){
+function fetchNotes() {
     try {
         var noteString = fs.readFileSync('data.json')
         return JSON.parse(noteString)
@@ -48,17 +48,17 @@ function fetchNotes(){
     }
 }
 
-function checkForTitle(array, title){
-  return array.filter((el)=>{
-    return el.title === title
-  })
+function checkForTitle(array, title) {
+    return array.filter((el) => {
+        return el.title === title
+    })
 }
 
-function sendNote(array){
+function sendNote(array) {
     fs.writeFileSync('data.json', JSON.stringify(array))
 }
 
-function error(){
+function error() {
     console.log('There was an error, Please try again');
 }
-module.exports = {addNote, listNotes, removeNote, getNote, error, checkForTitle}
+module.exports = { addNote, listNotes, removeNote, getNote, error, checkForTitle }
